75c4405bb5191096e46531664d7266ce793dce5c
kent0505 <otabekyusupov0550@gmail.com>
2025-01-09 10:31:41 +0500

    cors

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from fastapi          import FastAPI
-from contextlib       import asynccontextmanager
+from fastapi                 import FastAPI
+from contextlib              import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 
-from utils            import *
-from database         import create_tables
-from routers.fixtures import router as fixtures_router
-from routers.players  import router as players_router
+from utils                   import *
+from database                import create_tables
+from routers.fixtures        import router as fixtures_router
+from routers.players         import router as players_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,6 +15,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     lifespan=lifespan,
     swagger_ui_parameters={"defaultModelsExpandDepth": -1}
+)
+
+app.add_middleware(
+    middleware_class  = CORSMiddleware,
+    allow_credentials = True,
+    allow_origins     = ["*"],
+    allow_methods     = ["*"],
+    allow_headers     = ["*"],
 )
 
 app.include_router(fixtures_router, tags=["Fixtures"])
