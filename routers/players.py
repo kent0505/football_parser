a9c8080943078c2ce3789b2e461e70968843456d
kent0505 <otabekyusupov0550@gmail.com>
2025-01-09 11:16:32 +0500

    4

--- routers/players.py
+++ routers/players.py
@@ -55,58 +55,61 @@ async def get_players():
 
 @router.get("/player/{pid}")
 async def get_player(pid: int):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"{player_url}{pid}/player/stats", headers=headers) as response:
-            stats = {}
-            if response.status == 200:
-                html = await response.text()
-                soup = BeautifulSoup(html, "lxml")
-                try:
-                    first_name = soup.find("div", class_="player-header__name-first").get_text(strip=True)
-                    last_name = soup.find("div", class_="player-header__name-last").get_text(strip=True)
-                    # name = f"{first_name} {last_name}"
-                    number = soup.find("div", class_="player-header__player-number player-header__player-number--large").text.strip()
+    player = await database.get_player(pid)
+    if player:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"{player_url}{pid}/player/stats", headers=headers) as response:
+                stats = {}
+                if response.status == 200:
+                    html = await response.text()
+                    soup = BeautifulSoup(html, "lxml")
+                    try:
+                        first_name = soup.find("div", class_="player-header__name-first").get_text(strip=True)
+                        last_name = soup.find("div", class_="player-header__name-last").get_text(strip=True)
+                        name = f"{first_name} {last_name}"
+                        number = soup.find("div", class_="player-header__player-number player-header__player-number--large").text.strip()
 
-                    cols = soup.find("section", class_="player-overview__side-widget").find_all("div", class_="player-overview__col")
-                    age = cols[1].find("div", class_="player-overview__info").get_text(strip=True)
-                    height = cols[2].find("div", class_="player-overview__info").get_text(strip=True)
-                    team = cols[3].find("div", class_="player-overview__info").get_text(strip=True)
-                    position = cols[4].find("div", class_="player-overview__info").get_text(strip=True)
+                        cols = soup.find("section", class_="player-overview__side-widget").find_all("div", class_="player-overview__col")
+                        age = cols[1].find("div", class_="player-overview__info").get_text(strip=True)
+                        height = cols[2].find("div", class_="player-overview__info").get_text(strip=True)
+                        team = cols[3].find("div", class_="player-overview__info").get_text(strip=True)
+                        position = cols[4].find("div", class_="player-overview__info").get_text(strip=True)
 
-                    ul = soup.find("ul", class_="player-stats__stats-wrapper")
-                    for li in ul.find_all("li", class_="player-stats__stat"):
-                        for div in li.find_all("div", class_="player-stats__stat-value"):
-                            title = div.find(text=True, recursive=False).strip()
-                            value = div.find("span").get_text().strip()
-                            stats[title] = value
+                        ul = soup.find("ul", class_="player-stats__stats-wrapper")
+                        for li in ul.find_all("li", class_="player-stats__stat"):
+                            for div in li.find_all("div", class_="player-stats__stat-value"):
+                                title = div.find(text=True, recursive=False).strip()
+                                value = div.find("span").get_text().strip()
+                                stats[title] = value
 
-                    player = await database.get_player(pid)
-                    if player:
+                     
                         await database.edit_player(
                             pid, 
-                            f"{first_name} {last_name}", 
+                            name, 
                             position, 
                             team, 
                             number,
                         )
 
-                except Exception as e:
-                    logging.error(e)
-            return {
-                "status": response.status,
-                "player": {
-                    "name": f"{first_name} {last_name}",
-                    "position": position,
-                    "team": team,
-                    "number": number,
-                    "age": age,
-                    "height": height,
-                    "yc": stats.get("Yellow cards", 0),
-                    "rc": stats.get("Red cards", 0),
-                    "fouls": stats.get("Fouls", 0),
-                    "shots": stats.get("Shots", 0),
-                    "goals": stats.get("Goals", 0),
-                    "passes": stats.get("Passes", 0),
-                    "tackles": stats.get("Tackles", 0),
-                },
-            }
+                        return {
+                            "status": response.status,
+                            "player": {
+                                "name": name,
+                                "position": position,
+                                "team": team,
+                                "number": number,
+                                "age": age,
+                                "height": height,
+                                "yc": stats.get("Yellow cards", 0),
+                                "rc": stats.get("Red cards", 0),
+                                "fouls": stats.get("Fouls", 0),
+                                "shots": stats.get("Shots", 0),
+                                "goals": stats.get("Goals", 0),
+                                "passes": stats.get("Passes", 0),
+                                "tackles": stats.get("Tackles", 0),
+                            },
+                        }
+                    except Exception as e:
+                        logging.error(e)
+                raise HTTPException(response.status, "error")
+    raise HTTPException(404, "player not found")
